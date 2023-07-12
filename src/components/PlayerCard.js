b1b59826bf908870395117e60f14c25da20fa762
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-12 21:16:59 +0300

    useState import

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
 const PlayerCard = ({ name, img, statistics }) => {
-    //? 👆 const {name, image, statistics} = props
-    const [showImage, setShowImage] = useState(true);
-    const handleClick = () => setShowImage(!showImage);
+  //? 👆 const {name, image, statistics} = props
+  const [showImage, setShowImage] = useState(true);
+  const handleClick = () => setShowImage(!showImage);
   return (
     <Card
-    //? onClick={() => setShowImage(!showImage)}
-    onClick={handleClick}
-    className="rounded-2 m-auto player-card" role="button">
-        {showImage ? (
-            <Card.Img variant="top" src={img} className="player-img" />
-        ) :
-            (
-                <ul className="m-auto">
-                    {statistics.map((item, index) => {
-                        return (
-                            <li className="h5 text-start list-unstyled" key={index}>🏀 {item}</li>
-                        )
-                    })}
-                </ul>
-            )}
-        <Card.Footer>
-            <Card.Title>{name}</Card.Title>
-        </Card.Footer>
+      //? onClick={() => setShowImage(!showImage)}
+      onClick={handleClick}
+      className="rounded-2 m-auto player-card"
+      role="button"
+    >
+      {showImage ? (
+        <Card.Img variant="top" src={img} className="player-img" />
+      ) : (
+        <ul className="m-auto">
+          {statistics.map((item, index) => {
+            return (
+              <li className="h5 text-start list-unstyled" key={index}>
+                🏀 {item}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      <Card.Footer>
+        <Card.Title>{name}</Card.Title>
+      </Card.Footer>
     </Card>
-  )
-}
+  );
+};
 
-export default PlayerCard
+export default PlayerCard;
