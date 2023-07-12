a2ee0d524dfa9947a29cf8393661a194cf54233c
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-12 20:49:12 +0300

    cardcontainer row-col

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,26 +1,27 @@
-import React, {useState} from 'react';
-import { Container } from 'react-bootstrap';
-import Form from "react-bootstrap/Form";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import {data} from "../helper/data"
+import { Col, Container, Row, Form } from "react-bootstrap"
+//! 👇 Since data is exported as "export const", we need to call it in curly braces while importing.
+import { data } from "../helpers/data"
+import PlayerCards from "./PlayerCards"
+import { useState } from "react";
 
 const CardContainer = () => {
     const [search,setSearch] = useState("")
-console.log(data)
-    const handleChange = (e) => {
-        console.log(e.target.value)
-        setSearch(e.target.value)
-    }
-    console.log(search)
+
   return (
     <>
-      <Form.Control type="search" placeholder="SearchPlayer... " onChange={handleChange} />
-      <Container className='p-3 rounded-4 card-container my-3'>
-        <Row xs={2} md={4} lg={6} className='justify-content-center g-3'>
-            <Col>1 of 2</Col>
-            <Col>2 of 2</Col>
-            <Col>3 of 3</Col>
+           <Form.Control
+                type="search"
+                placeholder="Search Player..."
+                className="w-50 m-auto"
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <Container className="card-container rounded-4 my-4 p-3">
+                <Row className="gap-3 justify-content-center">
+                    {data.filter((player) => player.name.toLowerCase().includes(search.trim().toLowerCase())).map((player, index) => (
+                        <Col md={6} lg={4} xl={3} key={index}>
+                            <PlayerCards {...player} />
+                        </Col>
+                    ))}
 
         </Row>
       </Container>
